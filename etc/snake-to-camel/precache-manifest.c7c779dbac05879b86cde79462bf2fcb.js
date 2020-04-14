e1f09e9ce70e574ee318790d697e5644b5c58930
William Burnham <burnham.william@gmail.com>
2020-04-14 16:15:56 +0200

    try deploy snake to camel react app

---
+++ etc/snake-to-camel/precache-manifest.c7c779dbac05879b86cde79462bf2fcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6701222cbfad143834d04486bda110a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8739de84ad367d3f66f9",
+    "url": "/static/css/2.533de302.chunk.css"
+  },
+  {
+    "revision": "70f7a6897660dc51dd84",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8739de84ad367d3f66f9",
+    "url": "/static/js/2.8a157f3e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8a157f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f7a6897660dc51dd84",
+    "url": "/static/js/main.0f72d6d4.chunk.js"
+  },
+  {
+    "revision": "e2b985c426899a9490a6",
+    "url": "/static/js/runtime-main.284151af.js"
+  }
+]);
